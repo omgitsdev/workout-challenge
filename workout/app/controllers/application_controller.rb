0d78f14dd250bda6953e58205a8f3185ab7c00c1
Peng Gao <puerile22@gmail.com>
2014-10-18 09:03:46 -0700

    able to log in by username

--- workout/app/controllers/application_controller.rb
+++ workout/app/controllers/application_controller.rb
@@ -9,5 +9,6 @@ class ApplicationController < ActionController::Base
 protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:sign_up) << :email
   end
 end
