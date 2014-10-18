916040c8885a7e678973705fd1fd33ccf7afd3bd
Peng Gao <puerile22@gmail.com>
2014-10-17 19:18:35 -0700

    added user login page

--- challengr/app/assets/javascripts/application.js
+++ challengr/app/assets/javascripts/application.js
@@ -17,3 +17,18 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+var user;
+$(document).ready(function() {
+  $('#create-new-user').click(function() {
+    var name = $('#new-user').val();
+    $.ajax({
+      type:'POST',
+      url:'/users',
+      data:{user:{username:name}}
+    });
+  });
+  if (user == null) {
+    $('.user-list').hide();
+    $('.new-user').show();
+  }
+});
