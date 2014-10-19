086c2b6f2d95ee743142a80026e7a0a0e8eff230
Dev Sethi <omgitsdev@gmail.com>
2014-10-19 10:51:27 -0700

    moved scripts back to js files, fixed requirements

--- workout/app/assets/javascripts/application.js
+++ workout/app/assets/javascripts/application.js
@@ -11,13 +11,14 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs  
+//= require jquery_ujs
 //= require foundation
 //= require turbolinks
 //= require angular
 //= require angular-resource
 //= require angular-animate
-//= require_tree ./angular
+//= require_tree .
+
 
 
 $(function(){ $(document).foundation(); });
--- workout/app/assets/javascripts/groups.js
+++ workout/app/assets/javascripts/groups.js
@@ -1,3 +1,18 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+// $(document).ready(function() {
+  $("#join-group").on('click', function(e) {
+    e.preventDefault();
+    var id = $('#user-id').val();
+    var group_id = $('#group-id').val();
+    $.ajax({
+        type: "PATCH",
+        url: '/users/' + id,
+        data: {id:id,user:{group_id:group_id}},
+    }).done(function(data) {
+        var newUser = data.username;
+        $('body').append("<p>" + newUser + " has joined the group.</p>");
+    });
+  });
+// });
