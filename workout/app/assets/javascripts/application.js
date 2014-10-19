334e4aeaad922a5764231bdfbaca4b215883f1a1
Peng Gao <puerile22@gmail.com>
2014-10-19 10:04:11 -0700

    shows a message when user join a group

--- workout/app/assets/javascripts/application.js
+++ workout/app/assets/javascripts/application.js
@@ -21,18 +21,3 @@
 
 
 $(function(){ $(document).foundation(); });
-$(document).ready(function() {
-  $("#join-group").on('click', function(e) {
-    e.preventDefault();
-    var id = $('#user-id').val();
-    var group_id = $('#group-id').val();
-    $.ajax({
-        type: "PATCH",
-        url: '/users/' + id,
-        data: {id:id,user:{group_id:group_id}},
-    }).done(function(data) {
-        var newUser = data.username;
-        $('body').append("<p>" + newUser + " has joined the group.</p>");
-    });
-  });
-});
